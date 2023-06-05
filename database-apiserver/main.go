fee590130863e0141a045e3fe00dca79425b8546
Pritam <pritam@appscode.com>
2023-06-05 15:51:01 +0600

    updated extended branch

--- database-apiserver/main.go
+++ database-apiserver/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/x509"
+	"flag"
 	"fmt"
 	"github.com/PritamDas17021999/extended-ApiServer/lib/certstore"
 	"github.com/PritamDas17021999/extended-ApiServer/lib/server"
@@ -17,17 +19,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	var proxy = false
+	flag.BoolVar(&proxy, "receive-proxy-request", proxy, "receive forwarded requests from apiserver")
+	flag.Parse()
 	fs := afero.NewOsFs()
 	store, err := certstore.NewCertStore(fs, "/tmp/extended-ApiServer")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	err = store.NewCA("database")
+	err = store.InitCA("database")
 	if err != nil {
 		log.Fatalln(err)
 	}
 	serverCert, serverKey, err := store.NewServerCertPair(cert.AltNames{
-		IPs: []net.IP{net.ParseIP("127.0.1.2")},
+		IPs: []net.IP{net.ParseIP("127.0.0.2")},
 	})
 	if err != nil {
 		log.Fatalln(err)
@@ -48,20 +53,68 @@ func main() {
 		log.Fatalln(err)
 	}
 
+	//---------------------------------------------
+	apiserverStore, err := certstore.NewCertStore(fs, "/tmp/extended-ApiServer")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if proxy {
+		err = apiserverStore.LoadCA("apiserver")
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+	//-----------------------------------------------
+
+	//-----------------------------------------------
+	rhCACertPool := x509.NewCertPool()
+	rhStore, err := certstore.NewCertStore(fs, "/tmp/extended-ApiServer")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if proxy {
+		err = rhStore.LoadCA("requestheader")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		rhCACertPool.AppendCertsFromPEM(rhStore.CACertBytes())
+	}
+	//---------------------------------------------------
+
 	cfg := server.Config{
-		Address: "127.0.1.2:8444",
+		Address: "127.0.0.2:8444",
 		CACertFiles: []string{
 			store.CertFile("ca"),
 		},
 		CertFile: store.CertFile("tls"),
 		KeyFile:  store.KeyFile("tls"),
 	}
+	if proxy {
+		cfg.CACertFiles = append(cfg.CACertFiles, apiserverStore.CertFile("ca"))
+		cfg.CACertFiles = append(cfg.CACertFiles, rhStore.CertFile("ca"))
+	}
 	srv := server.NewGenericServer(cfg)
 	r := mux.NewRouter()
 	r.HandleFunc("/database/{resource}", func(w http.ResponseWriter, r *http.Request) {
+		user := "system:anonymous"
+		src := "-"
+		if len(r.TLS.PeerCertificates) > 0 {
+			opts := x509.VerifyOptions{
+				Roots:     rhCACertPool,
+				KeyUsages: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+			}
+
+			if _, err := r.TLS.PeerCertificates[0].Verify(opts); err != nil {
+				user = r.TLS.PeerCertificates[0].Subject.CommonName
+				src = "Client-Cert-CN"
+			} else {
+				user = r.Header.Get("X-Remote-User")
+				src = "X-Remote-User"
+			}
+		}
 		vars := mux.Vars(r)
 		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "Resource : %v\n", vars["resource"])
+		fmt.Fprintf(w, "Resource : %v requested by user [%s]=%s\n", vars["resource"], src, user)
 	})
 	r.HandleFunc("/", handler)
 	srv.ListenAndServe(r)
